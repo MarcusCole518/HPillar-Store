5b428943bf90d2c22b9e6c7ecc5db32548839e85
Marcus Little <mcolelittle@gmail.com>
2019-01-22 08:45:19 -0700

    add styled components, render product page title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import pillarLogo from '../pillarLogo.png';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+    <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
       <Link to='/'>
         <img src={pillarLogo} alt="store" className="navbar-brand"/>
       </Link>
@@ -18,12 +20,23 @@ export default class Navbar extends Component {
         </li>
       </ul>
       <Link to="/cart" className="ml-auto">
-        <button>
-          <i className="fa fa-cart-plus" aria-hidden="true" />
+        <ButtonContainer>
+          <span className="mr-2 ">
+            <i className="fa fa-cart-plus" aria-hidden="true" />
+          </span>
           my cart
-        </button>
+        </ButtonContainer>
       </Link>
-      </nav>
+    </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize !important;
+  }
+`
